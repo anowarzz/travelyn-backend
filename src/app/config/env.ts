ba3d296cbbf38ac22336b0faf879dd21d66f0e1a
Anowar Hosen <anowarhosen444@gmail.com>
2025-07-12 18:09:39 +0600

    refactor: separate checkAuth utils and add jwt secrets to env

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -33,8 +33,8 @@ const loadEnvVariables = (): EnvConfig => {
     DB_URL: process.env.DB_URL as string,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
-    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_SECRET as string,
-    BCRYPT_SALT_ROUND: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
   };
 };
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,34 +5,13 @@ import AppError from "../../errorHelpers/appError";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { verifyToken } from "../../utils/jwt";
 import { userControllers } from "./user.controller";
-import { Role } from "./user.interface";
 import { createUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router();
 
-const checkAuth =
-  (...authRoles: string[]) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const accessToken = req.headers.authorization;
 
-      if (!accessToken) {
-        throw new AppError(403, "No Access Token Found");
-      }
-
-      const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET);
-
-      const userRole = (verifiedToken as JwtPayload).role;
-
-      if (userRole !== Role.ADMIN) {
-        throw new AppError(403, "Unauthorized access");
-      }
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  };
 
 router.post(
   "/register",
@@ -40,6 +19,6 @@ router.post(
   userControllers.createUser
 );
 
-router.get("/all-users", userControllers.getAllUsers);
+router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN)  ,userControllers.getAllUsers);
 
 export const UserRoutes = router;
