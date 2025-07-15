5e0a914b395ed5419016361973f32f7bc9aa4625
Anowar Hosen <anowarhosen444@gmail.com>
2025-07-15 14:20:28 +0600

    feat: implement passport js and complete config

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import expressSession from "express-session";
+import passport from "passport";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 
+
+// create app
 const app = express();
 
+// middlewares
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET || "default_secret",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+// router
 app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
