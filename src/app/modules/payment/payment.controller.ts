c46fe95358abad48876ac30749827008a031c538
Anowar Hosen <anowarhosen444@gmail.com>
2025-07-28 19:48:03 +0600

    feat: implement fail and cancel payment api

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -2,18 +2,19 @@ import { Request, Response } from "express";
 import { envVars } from "../../config/env";
 import { catchAsync } from "../../utils/catchAsync";
 import { PaymentService } from "./payment.service";
+import { sendResponse } from "../../utils/sendResponse";
 
 // Initialize payment
-// const initPayment = catchAsync(async (req: Request, res: Response) => {
-//   const bookingId = req.params.bookingId;
-//   const result = await PaymentService.initPayment(bookingId as string);
-//   sendResponse(res, {
-//     statusCode: 201,
-//     success: true,
-//     message: "Payment done successfully",
-//     data: result,
-//   });
-// });
+const initPayment = catchAsync(async (req: Request, res: Response) => {
+  const bookingId = req.params.bookingId;
+  const result = await PaymentService.initPayment(bookingId as string);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Payment done successfully",
+    data: result,
+  });
+});
 
 // payment success
 const successPayment = catchAsync(async (req: Request, res: Response) => {
@@ -29,13 +30,35 @@ const successPayment = catchAsync(async (req: Request, res: Response) => {
 });
 
 // payment failure
-const failPayment = catchAsync(async (req: Request, res: Response) => {});
+const failPayment = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await PaymentService.failPayment(
+    query as Record<string, string>
+  );
+
+  if (!result.success) {
+    res.redirect(
+      `${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
+});
 
 // cancel payment
-const cancelPayment = catchAsync(async (req: Request, res: Response) => {});
+const cancelPayment = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await PaymentService.cancelPayment(
+    query as Record<string, string>
+  );
+
+  if (!result.success) {
+    res.redirect(
+      `${envVars.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+    );
+  }
+});
 
 export const PaymentController = {
-  // initPayment,
+  initPayment,
   successPayment,
   failPayment,
   cancelPayment,
