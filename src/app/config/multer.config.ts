51e289931bb4acb07caf1725018f8a97c30f7c2c
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-06 12:28:19 +0600

    feat: cofigure multer and cloudinary for image upload

---
+++ src/app/config/multer.config.ts
@@ -0,0 +1,30 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { cloudinaryUpload } from "./cloudinary.config";
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinaryUpload,
+  params: {
+    public_id: (req, file) => {
+      const fileName = file.originalname
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/\./g, "-")
+        .replace(/[^a-z0-9-]/g, "");
+
+      const extension = file.originalname.split(".").pop();
+
+      const uniqueFileNmae =
+        Math.random().toString(36).substring(2) +
+        "-" +
+        Date.now() +
+        "-" +
+        fileName +
+        "." +
+        extension;
+      return uniqueFileNmae;
+    },
+  },
+});
+
+export const multerUpload = multer({ storage: storage });
