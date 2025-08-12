cf771596c1af4791095ceb2b6ba0ddc88a19bc7b
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-12 11:22:00 +0600

    fix(auth): update secure true for setcookie

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { BookingRoutes } from "../modules/booking/booking.route";
 import { DivisionRoutes } from "../modules/division/division.route";
+import { OtpRoutes } from "../modules/otp/otp.route";
 import { PaymentRoutes } from "../modules/payment/payment.route";
 import { TourRoutes } from "../modules/tour/tour.route";
 import { UserRoutes } from "../modules/user/user.route";
@@ -32,6 +33,10 @@ const moduleRoutes = [
   {
     path: "/payment",
     route: PaymentRoutes,
+  },
+  {
+    path: "/otp",
+    route: OtpRoutes,
   },
 ];
 
