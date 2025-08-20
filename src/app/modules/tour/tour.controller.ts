e286d7498160735bacf0018731f3072ee5a1a9cc
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-20 10:07:39 +0600

    feat: implement multiple image upload while create a tour

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { ITour } from "./tour.interface";
 import { TourService } from "./tour.service";
 
 /*/ TOURS /*/
 
 // create tour
 const createTour = catchAsync(async (req: Request, res: Response) => {
-  const result = await TourService.createTour(req.body);
+  const payload: ITour = {
+    ...req.body,
+    images: (req.files as Express.Multer.File[])?.map((file) => file.path),
+  };
+
+  const result = await TourService.createTour(payload);
   sendResponse(res, {
     statusCode: 201,
     success: true,
